b1f7db49b195b7b3242bec9189da72c01b935de7
stefanpate <stefanpate94@gmail.com>
2024-12-16 16:37:48 -0600

    Set up filepaths configs. Sketched out nb for initial exploratory analysis incl getting r-hop substructures

--- cgr/draw.py
+++ cgr/draw.py
@@ -2,11 +2,24 @@ from rdkit import Chem
 from rdkit.Chem import Draw
 
 
-def draw_reaction(rxn: str, sub_img_size: tuple = (300, 200), use_smiles: bool = True):    
+def draw_reaction(rxn: str, sub_img_size: tuple = (300, 200), use_smiles: bool = True) -> str:
+    '''
+    Draw reaction to svg string
+
+    Args
+    -----
+    rxn:str
+        Reaction SMARTS
+    sub_img_size:tuple
+        width by height
+    use_smiles:bool
+        If True, is more explicit about double
+        bond location in drawing
+    '''
     rxn = Chem.rdChemReactions.ReactionFromSmarts(rxn, useSmiles=use_smiles)
     return Draw.ReactionToImage(rxn, useSVG=True, subImgSize=sub_img_size)
 
-def draw_molecule(smiles: str, size: tuple = (200, 200), hilite_atoms : tuple = tuple()) -> str:
+def draw_molecule(smiles: str, size: tuple = (200, 200), hilite_atoms: tuple = tuple(), draw_options: dict = {}) -> str:
     '''
     Draw molecule to svg string
 
@@ -20,9 +33,9 @@ def draw_molecule(smiles: str, size: tuple = (200, 200), hilite_atoms : tuple = 
         (width, height)
     hilite_atoms:tuple
         Atom indices to highlight
-    auto_scl:bool
-        If True, scales molecule image width proportional
-        to log(# of atoms)
+    draw_options:dict
+        Key-value pairs to set fields of 
+        rdkit.Chem.Draw.drawOptions object
     '''
     mol = Chem.MolFromSmiles(smiles)
     
@@ -31,9 +44,19 @@ def draw_molecule(smiles: str, size: tuple = (200, 200), hilite_atoms : tuple = 
         mol = Chem.MolFromSmiles(smiles, sanitize=False)
 
     drawer = Draw.MolDraw2DSVG(*size)
+    _draw_options = drawer.drawOptions()
+    for k, v in draw_options.items():
+        try:
+            setattr(_draw_options, k, v)
+        except Exception as e:
+            print(e)
+
     drawer.DrawMolecule(mol, highlightAtoms=hilite_atoms)
     
     drawer.FinishDrawing()
     img = drawer.GetDrawingText()
 
     return img
+
+if __name__ == '__main__':
+    draw_molecule('CCO')
--- cgr/filepaths.py
+++ cgr/filepaths.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from omegaconf import OmegaConf
 
 project_dir = Path(__file__).parents[1]
-filepaths = OmegaConf.load(project_dir / "configs" / "filepaths" / "base.yaml")
+filepaths = OmegaConf.load(project_dir / "configs" / "filepaths" / "filepaths.yaml")
 
 for k in filepaths.keys():
     filepaths[k] = Path(filepaths[k])
