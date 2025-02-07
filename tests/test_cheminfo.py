0a08b4fa7591a789dadc484bf37a221f65fe0131
stefanpate <stefanpate94@gmail.com>
2025-02-06 18:12:38 -0600

    Decoupled rmcs from rmcs score on molecule level. Added option to be more lenient on ring membership

--- tests/test_cheminfo.py
+++ tests/test_cheminfo.py
@@ -23,3 +23,51 @@ def test_fingerprinter():
     ac = mfper.atom_counts
     bim = mfper.bit_info_map
     a2b = mfper.atom_to_bits
+
+# TODO: construct test
+# import json
+# from cgr.filepaths import filepaths
+# from collections import defaultdict
+# import pandas as pd
+
+# krs = filepaths.data / "raw" / "sprhea_240310_v3_mapped_no_subunits.json"
+# with open(krs, 'r') as f:
+#     krs = json.load(f)
+
+# decarb = {k: v for k,v  in krs.items() if v['min_rule'] == 'rule0024'}
+# print(len(decarb))
+
+# max_hops = 3
+# vec_len = 2**12
+# mfper = MorganFingerprinter(radius=max_hops, length=vec_len, allocate_ao=True)
+# rc_dist_ub = None
+# n_samples = len(decarb)
+# data = defaultdict(list)
+# for rid, rxn in decarb.items():
+#     rc = rxn['reaction_center'][0]
+#     smiles = rxn['smarts'].split('>>')[0]
+#     mol = Chem.MolFromSmiles(smiles)
+#     _ = mfper.fingerprint(mol, reaction_center=rc, rc_dist_ub=rc_dist_ub)
+#     bim = mfper.bit_info_map
+
+
+#     for bit_idx, examples in bim.items():
+#         for (central_aidx, radius) in examples:
+#             data['feature_id'].append(bit_idx)
+#             data['sample_id'].append(rid)
+
+#             sub_idxs, sub_mol, sub_smi = extract_subgraph(mol, central_aidx, radius)
+
+#             data['sub_idxs'].append(sub_idxs)
+#             data['sub_smi'].append(sub_smi)
+#             data['saturated'].append(is_subgraph_saturated(mol, rc, sub_idxs))
+#             data['only_carbon'].append(has_subgraph_only_carbons(mol, rc, sub_idxs))
+
+# raw_subgraphs = pd.DataFrame(data)
+
+# resolved, embeddings = resolve_bit_collisions(raw_subgraphs, vec_len)
+# print()
+
+# for elt in resolved['feature_id'].unique():
+#     smiles = set(resolved.loc[resolved['feature_id'] == elt, 'sub_smi'].to_list())
+#     assert len(smiles) == 1
