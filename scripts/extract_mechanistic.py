e7e8a80c53a1185203f1ee9ac4ab712997db935d
stefanpate <stefanpate94@gmail.com>
2025-04-11 16:38:42 -0500

    Regenerate mechanistic templates post-rhea alternate reaction fix. Small changes to processing incl using rc_to_str/nest to store nested atom indices like rc and mech atoms

--- scripts/extract_mechanistic.py
+++ scripts/extract_mechanistic.py
@@ -3,13 +3,14 @@ from omegaconf import DictConfig
 from cgr.template import extract_reaction_template
 from pathlib import Path
 import pandas as pd
-from ergochemics.mapping import rc_to_str, rc_to_nest
+from ergochemics.mapping import rc_to_nest
 from collections import defaultdict
+import logging
+
+log = logging.getLogger(__name__)
 
 @hydra.main(version_base=None, config_path='../configs', config_name='extract_mechanistic')
 def main(cfg: DictConfig):
-
-    to_nested_lists = lambda x: [[arr.tolist() for arr in side] for side in x]
 
     mm = pd.read_parquet(
     Path(cfg.filepaths.raw_data) / cfg.src_file
@@ -17,18 +18,21 @@ def main(cfg: DictConfig):
 
     templates = defaultdict(list)
     for _, row in mm.iterrows():
-        rc = to_nested_lists(row['reaction_center'])
-        mech_atoms = to_nested_lists(row['mech_atoms'])
+        rc = rc_to_nest(row['reaction_center'])
+        mech_atoms = rc_to_nest(row['mech_atoms'])
         am_smarts = row['am_smarts']
-        template = extract_reaction_template(rxn=am_smarts, atoms_to_include=mech_atoms, reaction_center=rc[0])
-        templates[template].append((row["entry_id"], row['mechanism_id']))
+        try:
+            template = extract_reaction_template(rxn=am_smarts, atoms_to_include=mech_atoms[0], reaction_center=rc[0])
+            templates[template].append((row["entry_id"], row['mechanism_id']))
+        except Exception as e:
+            log.info(f"Error extracting template for entry_id {row['entry_id']}: {e}")
 
     tmp = []
-    for template, ems in templates.items():
+    for i, (template, ems) in enumerate(templates.items()):
         entries, mechs = zip(*ems)
-        tmp.append((template, list(entries), list(mechs)))
+        tmp.append((i, template, list(entries), list(mechs)))
 
-    df = pd.DataFrame(tmp, columns=["template", "entry_id", "mechanism_id"])
+    df = pd.DataFrame(tmp, columns=["id", "smarts", "entry_id", "mechanism_id"])
     df.to_csv(Path(cfg.filepaths.processed_data) / "mechanistic_reaction_templates.csv", sep=',', index=False)
 
 if __name__ == '__main__':
