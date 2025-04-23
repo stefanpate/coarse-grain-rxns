39ee49c6735f5d4a57dcb74e04932d9f6f7d222b
stefanpate <stefanpate94@gmail.com>
2025-04-23 18:35:14 -0500

    Re-generated mechanistic rules post update to mech labeling script

--- scripts/extract_mechanistic.py
+++ scripts/extract_mechanistic.py
@@ -12,20 +12,17 @@ log = logging.getLogger(__name__)
 @hydra.main(version_base=None, config_path='../configs', config_name='extract_mechanistic')
 def main(cfg: DictConfig):
 
-    mm = pd.read_parquet(
+    distilled_mech = pd.read_parquet(
     Path(cfg.filepaths.raw_data) / cfg.src_file
     )
 
     templates = defaultdict(list)
-    for _, row in mm.iterrows():
+    for _, row in distilled_mech.iterrows():
         rc = rc_to_nest(row['reaction_center'])
         mech_atoms = rc_to_nest(row['mech_atoms'])
         am_smarts = row['am_smarts']
-        try:
-            template = extract_reaction_template(rxn=am_smarts, atoms_to_include=mech_atoms[0], reaction_center=rc[0])
-            templates[template].append((row["entry_id"], row['mechanism_id']))
-        except Exception as e:
-            log.info(f"Error extracting template for entry_id {row['entry_id']}: {e}")
+        template = extract_reaction_template(rxn=am_smarts, atoms_to_include=mech_atoms[0], reaction_center=rc[0])
+        templates[template].append((row["entry_id"], row['mechanism_id']))
 
     tmp = []
     for i, (template, ems) in enumerate(templates.items()):
@@ -33,7 +30,7 @@ def main(cfg: DictConfig):
         tmp.append((i, template, list(entries), list(mechs)))
 
     df = pd.DataFrame(tmp, columns=["id", "smarts", "entry_id", "mechanism_id"])
-    df.to_csv(Path(cfg.filepaths.processed_data) / "mechanistic_rules.csv", sep=',', index=False)
+    df.to_csv("mechanistic_rules.csv", sep=',', index=False)
 
 if __name__ == '__main__':
     main()
