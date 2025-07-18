0c4785624592116ef70fd96956d614eacb0a15db
stefanpate <stefanpate94@gmail.com>
2025-07-18 17:07:24 -0500

    Add calibration option to train.py

--- scripts/train.py
+++ scripts/train.py
@@ -11,18 +11,34 @@ import pandas as pd
 import numpy as np
 from ergochemics.mapping import rc_to_nest
 from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
 import logging
+import pickle
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.frozen import FrozenEstimator
 from cgr.ml import (
     GNN,
     FFNPredictor,
     LinearPredictor,
     collate_batch,
     sep_aidx_to_bin_label,
-    calc_bce_pos_weight
+    calc_bce_pos_weight,
+    SklearnGNN
 )
 
 current_dir = Path(__file__).parent.parent.resolve()
 log = logging.getLogger(__name__)
+
+def reformat_data_for_calibration(dataset) -> tuple[list, np.ndarray]:
+    """Reformat data for sklearn calibration compatibility."""
+    refmt_dataset = []
+    ys = []
+    for i, (x, y) in enumerate(dataset):
+        ys.append(y)
+        for _ in range(y.shape[0]):
+            refmt_dataset.append((x, i))
+    refmt_y = np.vstack(ys).flatten()
+    return refmt_dataset, refmt_y
 
 @hydra.main(version_base=None, config_path=str(current_dir / "configs"), config_name="train")
 def main(cfg: DictConfig):
@@ -51,7 +67,7 @@ def main(cfg: DictConfig):
     log.info("Loading & preparing data")
     df = pd.read_parquet(
         Path(cfg.mechinformed_mapped_rxns)
-    )
+    )[:1000] # TODO: remove limit
 
     # Prep data
     df["template_aidxs"] = df["template_aidxs"].apply(rc_to_nest)
@@ -70,6 +86,15 @@ def main(cfg: DictConfig):
 
     # Featurize
     featurizer = featurizers.CondensedGraphOfReactionFeaturizer(mode_=cfg.model.featurizer_mode, atom_featurizer=featurizers.MultiHotAtomFeaturizer.v2())
+    
+    # Extra split & featurization for calibration
+    if cfg.do_calibrate:
+        train_X, calib_X, train_y, calib_y = train_test_split(
+            train_X, train_y, test_size=cfg.calibration_fraction, random_state=cfg.data.split_seed
+        )
+        calib_dataset = list(zip(data.ReactionDataset(calib_X, featurizer=featurizer), calib_y))
+        calib_dataset, calib_y = reformat_data_for_calibration(calib_dataset)
+
     train_dataset = list(zip(data.ReactionDataset(train_X, featurizer=featurizer), train_y))
     test_dataset = list(zip(data.ReactionDataset(test_X, featurizer=featurizer), test_y))
     train_dataloader = DataLoader(train_dataset, batch_size=cfg.training.batch_size, shuffle=True, collate_fn=collate_batch)
@@ -94,7 +119,7 @@ def main(cfg: DictConfig):
 
     # Logging
     logger = MLFlowLogger(
-        experiment_name="outer_splits",
+        experiment_name=cfg.experiment_name,
         tracking_uri="file:" + cfg.filepaths.mlruns,
         log_model=True,
         tags={"source": "train.py"},
@@ -108,10 +133,24 @@ def main(cfg: DictConfig):
         trainer = L.Trainer(max_epochs=cfg.training.max_epochs, logger=logger, accelerator="auto", devices=1)
         trainer.fit(model=model, train_dataloaders=train_dataloader)
         trainer.test(model=model, dataloaders=test_dataloader)
-        test_output = trainer.predict(model=model, dataloaders=test_dataloader)
 
-        # Format preds
-        y_pred = np.vstack([batch.cpu().numpy() for batch in test_output])
+        if cfg.do_calibrate:
+            calib_trainer = L.Trainer(logger=None, accelerator="auto", devices=1)
+            calib_skgnn = CalibratedClassifierCV(
+                FrozenEstimator(
+                    SklearnGNN(model=model, message_passing=mp, predictor=pred_head, trainer=calib_trainer)
+                )
+            )
+            calib_skgnn.fit(calib_dataset, calib_y)
+
+            test_dataset, _ = reformat_data_for_calibration(test_dataset) # Reformat here too not interfere w/ trainer above
+            y_pred = calib_skgnn.predict_proba(test_dataset)[:, 1].reshape(-1, 1) # 2nd row has P(y=1)
+
+        else:
+            test_output = trainer.predict(model=model, dataloaders=test_dataloader)
+            y_pred = np.vstack([batch.cpu().numpy() for batch in test_output])
+
+        # Format preds for dataframe
         aidxs = np.vstack([np.arange(elt.shape[0]).reshape(-1, 1) for elt in test_y], dtype=np.int32)
         y = np.vstack(test_y)
         df_rxn_ids = []
@@ -134,8 +173,20 @@ def main(cfg: DictConfig):
             sep='/'
         ).to_dict(orient='records')[0]
         mlflow.log_params(flat_resolved_cfg)
-        pred_df.to_parquet(Path(mlflow.get_artifact_uri().removeprefix("file:")) / "predictions.parquet", index=False)
-        mlflow.log_artifact(Path(mlflow.get_artifact_uri().removeprefix("file:")) / "predictions.parquet")
+        artifact_path = Path(mlflow.get_artifact_uri().removeprefix("file:"))
+
+        # Save preds
+        pred_df.to_parquet(artifact_path / "predictions.parquet", index=False)
+        mlflow.log_artifact(artifact_path / "predictions.parquet")
+
+        # Save calibrated model
+        if cfg.do_calibrate:
+            log.info("Saving calibrated model")
+            with open(artifact_path / "calibrated_model.pkl", "wb") as f:
+                pickle.dump(calib_skgnn, f)
+
+            mlflow.log_artifact(artifact_path / "calibrated_model.pkl")
+
         mlflow.log_artifact(Path(".hydra/config.yaml"))
 
 if __name__ == "__main__":
