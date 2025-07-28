50a8b2123048800634e6a4d7c4005a6e312dbc5a
stefanpate <stefanpate94@gmail.com>
2025-07-28 18:43:01 -0500

    Configs for expansion experiment

--- scripts/expand.sh
+++ scripts/expand.sh
@@ -17,12 +17,11 @@
 script=/home/spn1560/coarse-grain-rxns/scripts/expand.py
 rules_sweep=(
     "mechinformed_rules_w_coreactants"
-    "imt_rules_w_coreactants"
-    "mechinferred_dt_2_rules_w_coreactants" 
-    "mechinferred_dt_3_rules_w_coreactants"
-    "mechinferred_dt_6_rules_w_coreactants"
-    "mechinferred_dt_15_rules_w_coreactants"
-    "mechinferred_dt_98_rules_w_coreactants"
+    "mechinferred_dt_01_rules_w_coreactants" 
+    "mechinferred_dt_02_rules_w_coreactants"
+    "mechinferred_dt_04_rules_w_coreactants"
+    "mechinferred_dt_13_rules_w_coreactants"
+    "mechinferred_dt_91_rules_w_coreactants"
     "rc_plus_0_rules_w_coreactants" 
     "rc_plus_1_rules_w_coreactants"
     "rc_plus_2_rules_w_coreactants"
