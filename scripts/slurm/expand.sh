bc1ef17c8a27b3edabfc0d0506a4b55bc3602aed
stefanpate <stefanpate94@gmail.com>
2026-04-29 11:02:10 -0500

    cp in updated rbc mappings

--- scripts/slurm/expand.sh
+++ scripts/slurm/expand.sh
@@ -21,19 +21,20 @@ rules_sweep=(
     # mechinferred_dt_106_rules_before_2015_direct_mcsa_only_w_coreactants
     # mechinferred_dt_244_rules_before_2015_direct_mcsa_only_w_coreactants
     # mechinferred_dt_961_rules_before_2015_direct_mcsa_only_w_coreactants
-    mechinferred_dt_956_rules_w_coreacatants
-    mechinferred_dt_224_rules_w_coreacatants
-    mechinferred_dt_112_rules_w_coreacatants
-    mechinferred_dt_039_rules_w_coreacatants
-    mechinferred_dt_019_rules_w_coreacatants
-    mechinformed_rules_w_coreacatants
-    imt_rules_w_coreactants
-    rdchiral_rules_w_coreactants
-    rc_plus_0_rules_w_coreactants
-    rc_plus_1_rules_w_coreactants
-    rc_plus_2_rules_w_coreactants
-    rc_plus_3_rules_w_coreactants
-    rc_plus_4_rules_w_coreactants    
+    # mechinferred_dt_956_rules_w_coreacatants
+    # mechinferred_dt_224_rules_w_coreacatants
+    # mechinferred_dt_112_rules_w_coreacatants
+    # mechinferred_dt_039_rules_w_coreacatants
+    # mechinferred_dt_019_rules_w_coreacatants
+    # mechinformed_rules_w_coreacatants
+    # imt_rules_w_coreactants
+    # rdchiral_rules_w_coreactants
+    # rc_plus_0_rules_w_coreactants
+    # rc_plus_1_rules_w_coreactants
+    # rc_plus_2_rules_w_coreactants
+    # rc_plus_3_rules_w_coreactants
+    # rc_plus_4_rules_w_coreactants
+    retrobiocat_rules_w_coreactants
 )
 starters=250728_benchmark_starters
 generations=1
