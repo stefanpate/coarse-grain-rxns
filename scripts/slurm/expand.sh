b331e324b028e903c252710a22708a2d74b93b32
stefanpate <stefanpate94@gmail.com>
2026-05-06 19:10:34 -0500

    slrum

--- scripts/slurm/expand.sh
+++ scripts/slurm/expand.sh
@@ -16,12 +16,12 @@
 # Args
 script=/home/spn1560/coarse-grain-rxns/scripts/expand.py
 rules_sweep=(
-    evodex_Cm_rules_original_w_coreactants
-    evodex_Dm_rules_original_w_coreactants
-    evodex_Em_rules_original_w_coreactants
-    # evodex_Cm_rules_before_2015_w_coreactants
-    # evodex_Dm_rules_before_2015_w_coreactants
-    # evodex_Em_rules_before_2015_w_coreactants
+    # evodex_Cm_rules_original_w_coreactants
+    # evodex_Dm_rules_original_w_coreactants
+    # evodex_Em_rules_original_w_coreactants
+    evodex_Cm_rules_before_2015_w_coreactants
+    evodex_Dm_rules_before_2015_w_coreactants
+    evodex_Em_rules_before_2015_w_coreactants
     # mechinferred_dt_035_rules_before_2015_direct_mcsa_only_w_coreactants
     # mechinferred_dt_059_rules_before_2015_direct_mcsa_only_w_coreactants
     # mechinferred_dt_106_rules_before_2015_direct_mcsa_only_w_coreactants
@@ -42,8 +42,8 @@ rules_sweep=(
     # rc_plus_4_rules_w_coreactants
     # retrobiocat_rules_w_coreactants
 )
-starters=250728_benchmark_starters
-generations=2
+starters=after_2015_cpds
+generations=1
 explicit_h=true
 processes=50 # MAKE SURE THIS MATCHES -n above
 
